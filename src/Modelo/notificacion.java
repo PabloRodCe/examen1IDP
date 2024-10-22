7c36e648690cfcc369508a420b24398bba0444c4
Isaac Sibaja <isaacsibaja901@gmail.com>
2024-10-22 11:53:38 -0600

    notificacion

--- src/Modelo/notificacion.java
+++ src/Modelo/notificacion.java
@@ -8,6 +8,33 @@ package Modelo;
  *
  * @author Student
  */
-public class notificacion {
+public class notificacion extends Estado{
+    private String Email;
+
+    public notificacion(String Email, Pedido pedido) {
+        super(pedido);
+        this.Email = Email;
+    }
+    
+
+    @Override
+    public String pendiente() {
+          System.out.println("Email to " + Email + ": Someone has performed " + eventType + " operation with the following file: " + file.getName());
+    }
+
+    @Override
+    public String cancelado() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String completado() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String entregado() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
