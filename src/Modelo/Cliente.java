6fbc0d1f741172646299f65504c0d161c50c5a76
Pablo Rodriguez <jorodriguezce@est.utn.ac.cr>
2024-10-22 10:28:03 -0600

    se actualiza cliente

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -9,42 +9,20 @@ package Modelo;
  * @author Student
  */
 public class Cliente {
-<<<<<<< Updated upstream
+
    private int id;
    private String nombre;
    private String apellido;
    String Torden;
+   
 
     public Cliente(int id, String nombre, String apellido) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
-=======
-    
-    private int id;
-    private int telefono;
-    private String Nombre;
-
-    public Cliente(int id, int telefono, String Nombre) {
-        this.id = id;
-        this.telefono = telefono;
-        this.Nombre = Nombre;
     }
 
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
-
-    public void setNombre(String Nombre) {
-        this.Nombre = Nombre;
->>>>>>> Stashed changes
-    }
-
-    public int getId() {
-        return id;
-    }
-
-<<<<<<< Updated upstream
+   
     public void setId(int id) {
         this.id = id;
     }
@@ -64,16 +42,8 @@ public class Cliente {
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-   
-=======
-    public int getTelefono() {
-        return telefono;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-    
-    
->>>>>>> Stashed changes
 }
+
+   
+
+
