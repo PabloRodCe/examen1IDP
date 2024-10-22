b1731e5e8187aa6216ab1f346a372e0a7fa1a7dd
Pablo Rodriguez <jorodriguezce@est.utn.ac.cr>
2024-10-22 11:18:06 -0600

    se agregan datos a las clases

---
+++ src/Modelo/listCliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Student
+ */
+public class listCliente extends Cliente {
+     ArrayList<String>listaCliente=new ArrayList<>();
+     
+     
+ private void AgregarCliente(){
+        listaCliente.add(Cliente);
+    }
+}
