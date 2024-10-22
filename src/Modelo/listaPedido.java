433b651c624f54f3cb67452e9a305478da26d09c
Pablo Rodriguez <jorodriguezce@est.utn.ac.cr>
2024-10-22 11:52:40 -0600

    se agrega arraylst

---
+++ src/Modelo/listaPedido.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Student
+ */
+public class listaPedido extends Pedido {
+     ArrayList<String>listaPedido=new ArrayList<>();
+    
+public void leerPedido(){
+listaPedido.get(Pedido);
+}
+
+
+}
